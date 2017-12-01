c18cbe377b93799407dd89a4f439d9f1ad2a780e
mo <meganannetteolson@yahoo.com>
2017-12-01 09:46:12 -0800

    Add admin functionality.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = Review.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      flash[:notice] = "Review Posted!"
+    else
+      flash[:alert] = "Something went wrong."
+    end
+    redirect_to product_path(@product)
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:content)
+  end
+
 end
