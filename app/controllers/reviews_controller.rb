a586cfb6013c46e144e1b029fc08752ce6826c02
mo <meganannetteolson@yahoo.com>
2017-12-01 11:28:51 -0800

    Add name to reviews.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,7 @@ class ReviewsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
-    @comment.user = current_user
+    @review.user = current_user
     if @review.save
       flash[:notice] = "Review Posted!"
     else
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,5 @@ RSpec.configure do |config|
       with.test_framework :rspec
       with.library :rails
     end
+  end
 end
