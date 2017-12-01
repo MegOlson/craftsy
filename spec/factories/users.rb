44ae30b55c94fcc3e48c209e921878d40e60e336
mo <meganannetteolson@yahoo.com>
2017-12-01 11:35:02 -0800

    90% test coverage.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    name("test123")
+    email("test123@test.com")
+    password("test123")
+    password_confirmation("test123")
   end
+
+  factory(:review) do
+    content("Test Review")
+  end
+
 end
