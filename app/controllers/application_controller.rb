07b2cc9a5f86c6dee0c236c5244b95c4c18811dc
mo <meganannetteolson@yahoo.com>
2017-12-01 09:15:20 -0800

    Add rails admin and display all products with clickability.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit({roles: [] }, :email, :name, :password, :password_confirmation)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'products#index'
 
   devise_for :users
+
+  resources :products
+
 end
